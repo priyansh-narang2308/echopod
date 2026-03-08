e600ae5b2189d6caa5f882d4286e8d4e3ec3c294
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-03-08 17:28:24 +0530

    feat: Podcast Index implemented and did its first call

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from 'react-native';
+import { fetchTrending } from '@/services/podcast-index';
+import { Button, Text, View } from 'react-native';
 
 export default function Home() {
+
+    const onPress = async () => {
+        const data = await fetchTrending()
+        console.log(JSON.stringify(data, null, 2))
+    }
+
     return (
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-xl font-bold">Home</Text>
+
+            <Button title='Fetch' onPress={onPress} />
         </View>
     );
 }
